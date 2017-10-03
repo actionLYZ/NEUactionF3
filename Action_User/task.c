5a3d83dbeba7eef771fc30a3c3c13fd630131b34
yushilingco <1071206032@qq.com>
2017-10-03 16:30:38 +0800

    准备测试发射

--- Action_User/task.c
+++ Action_User/task.c
@@ -192,15 +192,15 @@ void WalkTask(void)
 			fighting=1;
 		}
 //    USART_OUT(UART5,(u8*)"r%d\tl%d\r\n",(int)right,(int)left);
-		CountBall();
+//		CountBall();
 		//USART_OUT(UART5,"%d\t%d\t%d\r\n",(int)blindTime,(int)velocity,(int)photoElectricityCount);
 //		ReadActualVel(CAN2,RIGHT_MOTOR_WHEEL_ID);
 //		ReadActualVel(CAN2,LEFT_MOTOR_WHEEL_ID);
-//		ShootBallW(); 
+		ShootBallW(); 
 //		RunWithCamera1(2);
 //		USART_OUT(UART5,(u8*)"%d\t%d\t%d\r\n",(int)Position_t.X,(int)Position_t.Y,(int)Position_t.angle);
 //普通避障
-		if(ifEscape)
+/*		if(ifEscape)
 		{
 			carRun = 0;
 			
@@ -267,7 +267,7 @@ void WalkTask(void)
 			}
 		}
 
-		
+	*/	
 
 	}
 }
